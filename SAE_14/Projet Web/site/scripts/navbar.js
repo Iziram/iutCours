2bd7384a2ed0c5d2829d0eac080b2fd078ddd0a7
Iziram <contact@iziram.fr>
2021-12-13 17:31:34 +0100

    navbar finished

--- SAE_14/Projet Web/site/scripts/navbar.js
+++ SAE_14/Projet Web/site/scripts/navbar.js
@@ -1,21 +1,50 @@
-function navbar(active){
+function navbar(active = "index"){
+    const body = document.getElementsByTagName('body')[0];
+    const pages = [
+        "index",
+        "competences",
+        "interets",
+        "liensutiles",
+        "projetprofessionnel"
+    ]
+
     const nav = document.createElement('nav')
-        nav.className="navbar navbar-expend-lg navbar-dark bg-dark"
+        nav.className = "navbar navbar-dark bg-dark"
         const container = document.createElement('div')
-            container.className="container-fluid"
-            const a_navbar = document.createElement('a')
-                a_navbar.href = "index.html"
-                a_navbar.text = "Iziram"
-                a_navbar.className = "navbar-brand"
-            container.appendChild(a_navbar)
+            container.className = "container-fluid"
+            const brand = document.createElement('a')
+                brand.className = "navbar-brand"
+                brand.href="index.html"
+                brand.text = "Iziram"
+            container.appendChild(brand)
+
+            const button = new DOMParser().parseFromString(`
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            `,"text/xml")
+            container.appendChild(button.firstChild)
+
+            const collapse = document.createElement('div')
+                collapse.className = "collapse navbar-collapse"
+                collapse.id="navbar"
+                const ul = document.createElement('ul')
+                    ul.className = "navbar-nav me-auto mb-2"
+                    pages.forEach((page)=>{
+                        const li = document.createElement('li')
+                            li.className = "nav-item"
+                            const a = document.createElement('a')
+                                a.href = page+".html"
+                                a.className = "nav-link"
+                                if(active === page ){
+                                    a.classList.add("active")
+                                }
+                                a.innerHTML = page
+                            li.appendChild(a)
+                        ul.appendChild(li)
+                    })
+                collapse.appendChild(ul)
+            container.appendChild(collapse)
         nav.appendChild(container)
-        const collapse = document.createElement('button')
-            collapse.outerHTML = `<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>`
-        
-        nav.appendChild(collapse)
-
-
-        document.getElementsByTagName('body')[0].appendChild(nav)
+    body.appendChild(nav)
 }
