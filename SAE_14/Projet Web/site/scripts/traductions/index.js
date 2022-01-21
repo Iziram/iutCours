4f3f9abeed3e3a280760d03f558da4aca259dc7c
Iziram <matthiworld@gmail.com>
2022-01-21 09:08:10 +0100

    traduction index (no off canvas) ppf

---
+++ SAE_14/Projet Web/site/scripts/traductions/index.js
@@ -0,0 +1,88 @@
+const tradData = {
+    "fr":{
+        "subtitle": "Scroller pour me découvrir",
+        
+        "infos-t": "Qui suis-je ?",
+            "infos-t-a": `Je m'appelle Matthias Hartmann et j'ai 19 ans. Je suis un développeur (principalement Back-End). Je fais actuellement un BUT Réseaux et Télécommunications à Lannion. J'aime l'informatique et plus particulièrement la programmation.`,
+            "infos-t-b": `En ligne j'utilise le pseudo Iziram, vous pourrez me retrouvez sur de nombreux réseaux sociaux et platformes liés au développement (Github, Trello, LeetCode...)`,
+        
+        "compt-t":"Mes Compétences",
+            "compt-s-a":"Programmation",
+            "compt-s-b":"Gestion de projet",
+            "compt-s-c":"Systèmes",
+
+        "forma-t":"Mes diplômes et mes Formations",
+            "forma-ca-t":"Baccalauréat Général",
+                "forma-ca-la":"Année : 2020-2021",
+                "forma-ca-lb":"Spécialités Mathématiques et NSI (Numériques et Sciences Informatiques)",
+                "forma-ca-lc":"Options : Section Européen, DNL Maths (Discipline Non linguistique en Anglais) ",
+                "forma-ca-ld":"Mention : Bien",
+                "forma-ca-le":` Passé dans le lycée <a href="https://www.lacroixrouge-brest.fr/" target="_blank">La Croix Rouge - La Salle</a> à Brest`,
+            "forma-cb-t":"BUT Réseaux et Télécommunications",
+                "forma-cb-la":"Année(s) : 2021-Aujourd'hui",
+                "forma-ca-lb":`Passé dans l'<a href="https://iut-lannion.univ-rennes1.fr/" target="_blank">IUT de Lannion</a>`,
+
+        "exp-t":"Mes Expériences",
+            "exp-ca-t":"Stage en IUT GEII • 03/2019",
+                "exp-ca-s":`Stage fait en Seconde dans le cadre du projet "Découverte des métiers"`,
+            "exp-cb-t":"Développeur Java • 07/2020 - 09-2021",
+                "exp-cb-s":`Développement de plugins Minecraft (modifications serveur)`,
+            "exp-cc-t":"Création d'Extension VSCode • 11/2021 - Aujourd'hui",
+                "exp-cc-s":`Développement de l'Extension "<a href="https://marketplace.visualstudio.com/items?itemName=iziram.autodoxygen" target="_blank">AutoDoxyGen</a>"`,
+            "exp-cd-t":"Autres expériences",
+                "exp-cd-sa":`Vidéo : Mes points forts à travers un hobby ou une expériences`,
+                "exp-cd-sb":`Vidéo : Mes compétences et mes Projets `,
+
+
+        "int-t":"Mes centres d'intérets",
+            "int-a-t":"Programmation",
+                "int-a-s":`Je pratique l'escrime comme sport depuis maintenant 8 ans, l'escrime comporte 3 armes, je fais principalement de l'épée même si je maitrise aussi le sabre et le fleuret. Je fais beaucoup de comptétions au niveau régionnal mais il m'est aussi arrivé d'aller à deux compétitions
+                nationnales en 2018. Je suis aussi arbitre régionnal et Animateur (1ère formation qui permet d'enseigner l'escrime)`,
+            "int-b-t":"Escrime",
+                "int-b-s":`J'adore la programmation, je suis toujours à la recherche de nouveaux projets qui pourraient me faire découvrir de nouveaux outils, de nouveaux langages et de nouvelles façons d'aborder la programmation.
+                J'ai commencé en 2015 et je n'ai pas arrêté depuis. Je programme dans beaucoup de langages mais principalement en Java, Python et Javascript. `,
+    },
+    "en":{
+        "subtitle": "Scroll down to discover me",
+        
+        "infos-t": "Who am I?",
+            "infos-t-a": `My name is Matthias Hartmann and I am 19 years old. I am a developer (mainly Back-End). I'm currently doing a BUT Networks and Telecommunications in Lannion. I like computer science and especially programming.`,
+            "infos-t-b": `Online I use the nickname Iziram, you can find me on many social networks and platforms related to development (Github, Trello, LeetCode ...)`,
+        
+        "compt-t":"My Skills",
+            "compt-s-a":"Programmation",
+            "compt-s-b":"Project management",
+            "compt-s-c":"Systems",
+
+        "forma-t":"My diplomas",
+            "forma-ca-t":"General Baccalaureate",
+                "forma-ca-la":"Year: 2020-2021",
+                "forma-ca-lb":"Mathematics and NSI (Numerical and Computer Sciences) specialties",
+                "forma-ca-lc":"Options: European Section, DNL Math (Non Linguistic Discipline in English) ",
+                "forma-ca-ld":"Honors: Good",
+                "forma-ca-le":`Passed through <a href="https://www.lacroixrouge-brest.fr/" target="_blank">La Croix Rouge - La Salle</a> high school in Brest`,
+            "forma-cb-t":"GOAL Networks and Telecommunications",
+                "forma-cb-la":"Year(s) : 2021-Present",
+                "forma-cb-lb":`Past in the <a href="https://iut-lannion.univ-rennes1.fr/" target="_blank">IUT de Lannion</a>`,
+
+        "exp-t":"My Experiences",
+            "exp-ca-t":"Internship in IUT GEII - 03/2019",
+                "exp-ca-s":`Internship done in Seconde as part of the project "Discovery of jobs".`,
+            "exp-cb-t":"Java Developer - 07/2020 - 09-2021",
+                "exp-cb-s":`Minecraft plugins development (server modifications)`,
+            "exp-cc-t":"VSCode Extension Creation - 11/2021 - Today",
+                "exp-cc-s":`Development of the "<a href="https://marketplace.visualstudio.com/items?itemName=iziram.autodoxygen" target="_blank">AutoDoxyGen Extension</a>"`,
+            "exp-cd-t":"Other experiences",
+                "exp-cd-sa":`Video: My strengths through a hobby or an experience`,
+                "exp-cd-sb":`Video: My skills and my projects `,
+
+
+        "int-t":"My interests",
+            "int-a-t":"Fencing",
+                "int-a-s":`I practice fencing as a sport for 8 years now, fencing has 3 weapons, I mainly do epee even if I also master the saber and the foil. I do a lot of competitions at the regional level but I also happened to go to two national competitions in 2018. I am also a regional referee and an Animator (1st training that allows to teach fencing)`,
+            "int-b-t":"Programmation",
+                "int-b-s":`I love programming, I'm always looking for new projects that might introduce me to new tools, new languages and new ways of approaching programming.
+                I started in 2015 and haven't stopped since. I program in many languages but mainly in Java, Python and Javascript. `,
+    },
+    
+}
---
+++ SAE_14/Projet Web/site/scripts/traductions/ppf.js
@@ -0,0 +1,86 @@
+const tradData = {
+    "en":{
+        "heroTitle": "My Career Plan",
+        "heroText": "On this page you will find information about my future, whether it is the second year of BUT or further studies",
+    
+        //PPF
+        "t-1": "The second year of the Networks and Telecommunications program",
+        "t-2": "Between internships and work-study",
+        "t-3": "towards the international",
+        "t-4": "Further studies",
+
+        "t-a": `Before entering the second year, you have to choose a path between Cybersecurity and DevOps. 
+        For me the choice was quick.Indeed, I am particularly fond of the subject of Cybersecurity 
+        for quite a long time.J'ai pu, par exemple, faire une présentation en Terminal sur ce sujet et participer 
+        in online conferences or at the <a href="https://devfest.duboutdumonde.bzh/" target="_blank">DevFestBDM</a>. 
+        Thus, this choice of study remains in the continuity of my previous choices which aim to deepen my knowledge in 
+        my knowledge in Cybersecurity and more generally in computer science.`,
+
+        "t-b": `The second and third years are punctuated by professional experience, either in the form of internships or alternating studies. 
+        I decided that I wanted to focus my BUT years on the initial cursus. 
+        That is, I will not be doing a work-study but <strong>internships</strong> in semesters 4 and 6. 
+        In this way I will accumulate the general knowledge of the BUT which will allow me to enter engineering school with a general background. 
+        It is then only in engineering school that I will do a work-study program and that I will specialize.`,
+
+        "t-c": `Concerning the internships, I would like to do them internationally if possible because it is for me 
+        it is a very good way to get out of my comfort zone, to deepen my language skills, and to 
+        language skills, and form a professional network internationally. 
+        In addition, I would like to learn new cultures and see how companies abroad work. 
+        It Obviously, this kind of project requires a lot of preparation beforehand and will be difficult to set up but I will do my best to do it for the second year, 
+        the third year or year, the third year or in the best case to be able to do it for both years.`,
+
+        "t-d": `At the end of my third year at BUT, if all goes well, I will have graduated. I don't intend to
+        I do not intend to stop my studies. I am aiming for a minimum of 5 years of higher education with 
+        at the end a Master's degree in Computer Science or an engineering degree in Computer Science. 
+        For that there are many schools, I did not finish my research but I could already note some formations which 
+        but I have already been able to note a few courses that I like. 
+        There is firstly the <a href="https://www-ensibs.univ-ubs.fr/fr/index.html" target="_blank">ENSIBS</a> in Vannes and their curricula on cybersecurity and cyberdefense. 
+        There is also the <a href="https://www.enssat.fr/" target=_blank>ENSSAT</a> in Lannion which offers courses in Computer Science and Digital Systems Engineering. 
+        These two schools are my main objectives but future research may direct me may lead me to other French or even international schools. `,
+
+        "t-l": `More information about the BUT`,
+    },
+    "fr":{
+        "heroTitle": "Mon projet Professionnel",
+        "heroText": "Sur cette page vous retrouverez les informations relatives à mon futur, que ce soit la deuxième année de BUT ou bien les poursuites d'études",
+
+        //PPF
+        "t-1":"La Deuxième année de BUT Réseaux et Télécommunications",
+        "t-2":"Entre Stages et Alternances",
+        "t-3":"Direction l'International",
+        "t-4":"Poursuites d'études",
+
+        "t-a":`Avant de rentrer en deuxième année, il faut choisir un parcours entre Cybersécurité et DevOps. 
+        Pour moi le choix a été rapide. En effet j'affectionne particulièrement le sujet de la Cybersécurité 
+        depuis assez longtemps. J'ai pu, par exemple, faire une présentation en Terminal sur ce sujet et participer 
+        à des conférences en lignes ou au <a href="https://devfest.duboutdumonde.bzh/" target="_blank">DevFestBDM</a>. 
+        Ainsi ce choix de poursuite d'études reste dans la continuité de mes précédents choix qui ont pour but d'approfondir 
+        mes connaissances en Cybersécurité et plus généralement en informatique.`,
+        
+        "t-b":`la deuxième et troisième année sont rythmées par des expériences professionnelles 
+        que ce soit sous la forme de stages ou bien d’études en alternance. J'ai décidé de vouloir concentré mes années de BUT sur un apprentisage en formation initiale. 
+        C'est à dire que je ne ferai pas d'alternance mais des <strong>stages</strong> aux semestres 4 et 6. De cette façon j'accumulerai les connaissances générales de la formation 
+        ce qui me permettra d'entrer en école d'ingénieur avec un baguage général. Ce n'est alors qu'en école d'ingénieur que je ferai de l'alternance et que je me spécialiserai.`,
+        
+        "t-c":`Concernant les stages, je souhaiterai les faire à l’international si possible car c’est pour moi 
+        c’est une très bonne méthode de sortir de sa zone de confort, approfondir mes compétences 
+        linguistiques, et former un réseau professionnel à l’international. De plus, j’aimerai 
+        connaître de nouvelles cultures et voir comment fonctionne les entreprises à l’étranger. Il 
+        est évident que ce genre de projet nécessite beaucoup de préparations en amont et sera 
+        difficile à mettre en place mais je ferai mon possible afin de le réaliser pour la deuxième 
+        année, la troisième année ou dans le meilleur des cas pouvoir le faire pour les deux années.`,
+        
+        "t-d":`A la fin de ma troisième année de BUT, si tout se passe bien, j’aurai obtenu mon diplôme. Je ne
+        compte pas pour autant arrêter mes études. Je vise au minimum un niveau Bac+5 avec 
+        l’obtention d’un Master en Informatique ou bien un diplôme d’ingénieur en informatique. 
+        Pour cela il existe de nombreuses écoles, je n’ai pas terminé mes recherches mais j’ai déjà 
+        pu noter quelques formations qui me plaisent. Il y a premièrement l’<a href="https://www-ensibs.univ-ubs.fr/fr/index.html" target="_blank">ENSIBS</a> à Vannes et 
+        leurs cursus sur la cybersécurité et la cyberdéfense. Il y a aussi l’<a href="https://www.enssat.fr/" target=_blank>ENSSAT</a> à Lannion qui 
+        propose des formations d’ingénieur en Informatique et d’ingénieur en Systèmes 
+        Numériques. Ces deux écoles sont mes principaux objectifs mais de futur recherches 
+        peuvent m’orienter vers d’autres écoles françaises ou même internationales. `,
+        "t-l":`Plus d'informations sur le BUT`,
+        
+    },
+    
+}
