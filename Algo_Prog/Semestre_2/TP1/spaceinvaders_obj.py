a92d08fd5bce156ebd39ffbc318cc12dd32a0ec8
Iziram <matthiworld@gmail.com>
2022-02-24 23:18:48 +0100

    space invaders version poo(quasiment fini)

--- Algo_Prog/Semestre_2/TP1/spaceinvaders_obj.py
+++ Algo_Prog/Semestre_2/TP1/spaceinvaders_obj.py
@@ -1,10 +1,14 @@
 from typing import List, Dict, Tuple
 from os import system
 from time import sleep
+from saisiCar import SaisiCar
+
 class Plateau:
     largeur: int = 30
     hauteur: int = 20
     normalC : str = "\033[0;37;40m"
+
+    typeTirs : List[str] = [":","+","|"]
     def afficherLigne():
         print("-"*Plateau.largeur)
         
@@ -15,14 +19,19 @@ class Plateau:
         f'  {jeu.score:5}  {jeu.vie : 5}     {jeu.niveau : 5}')
         Plateau.afficherLigne()
         
-    def afficherPlateau(self,jeu):
+    def afficherPlateau(self,jeu, posTir:Tuple[int,int] = None):
         for ligne in range(Plateau.hauteur):
             for col in range(Plateau.largeur):
+                if posTir != None and col == posTir[0] and ligne < Plateau.hauteur -1 and ligne > posTir[1]:
+                    print(Plateau.typeTirs[jeu.vaisseau.nivTir -1], end=Plateau.normalC)
+                    continue
+                if(ligne == Plateau.hauteur - 1 and jeu.vaisseau.posX == col):
+                    print(jeu.vaisseau, end=Plateau.normalC)
+                    continue
                 pos : List[Alien] = [a for a in jeu.aliens if a.position() == (col, ligne)]
                 alien : Alien or str = pos[0] if len(pos) > 0 else " " 
                 print(alien, end=Plateau.normalC)
-                if(ligne == Plateau.hauteur - 1 and jeu.vaisseau.posX == col):
-                    print(jeu.vaisseau, end=Plateau.normalC)
+                
             print("\n", end="")
 
 class Vaisseau:
@@ -46,6 +55,21 @@ class Vaisseau:
     def __str__(self):
         return f'{self.vaisseauC}#'
 
+class Alien:
+    alienC : str = "\033[1;32;40m"
+    def __init__(self, x:int, y:int, nivTir : int = 0):
+        self.posX = x
+        self.posY = y
+        self.sens = True
+        self.nivTir = nivTir
+    def __repr__(self):
+        return f'en position ({self.posX}, {self.posY}) - tir {self.nivTir}'
+    def __str__(self):
+        return f'{self.alienC}@' 
+
+    def position(self) -> Tuple[int, int]:
+        return (self.posX, self.posY)
+
 class PartieJeu:
     modeEmploi : str = """Bienvenue Dans SpaceInvaders:
         Utilisez els touches o,k,m pour tirer, se déplacer à gauche ou à droite"""
@@ -59,6 +83,7 @@ class PartieJeu:
         self.initialiserAliens(nombreAlien)
     
     def initialiserAliens(self, nbAliens:int, nbLignes:int = 10):
+        self.aliens = []
         y = -1 
         for i in range(nbAliens):
             x = i % nbLignes
@@ -91,41 +116,64 @@ class PartieJeu:
                     a.posX += 1
                 else:
                     a.posX -= 1
-                    
     
-    def boucleDeJeu(self):
+    def alienPlusBas(self) -> Alien:
+        if(len(self.aliens) > 0 ):
+            return max(self.aliens, key = lambda alien:alien.posY)
+        return None
+    def aliensSurColonne(self, colonne:int) -> List[Alien]:
+        aliens = [a for a in self.aliens if a.posX == colonne]
+        
+        return aliens
+    def tuerAliens(self, aliens:List[Alien], nombre:int) -> int:
+        if(aliens != None or len(aliens) > 0):
+            for i in range(nombre):
+                if len(aliens) > 0:
+                    self.aliens.remove(aliens.pop())
+            return nombre
+        return 0
+    
+    def boucleDeJeu(self, plateau:Plateau):
         action : str = ""
+        kb = SaisiCar()
         while self.vie > 0 and action !="q":
-            system("clear")
-        plateau.afficherBandeau(partie)
-        plateau.afficherPlateau(partie)
-        partie.deplacementAliens()
-        sleep(.1)
+            system("cls")
+            action = kb.recupCar(["m", "k", "o", "q"])
+            if(action != None and action != ""):
+                self.vaisseau.action(action)
+            
+            plateau.afficherBandeau(partie)
+            if (self.vaisseau.tir == True):
+                aliens : List[Alien] = self.aliensSurColonne(self.vaisseau.posX)
+                self.score += 5 * self.tuerAliens(aliens, self.vaisseau.nivTir)  
+                self.vaisseau.tir = False
+                if len(aliens) > 0:
+                    plateau.afficherPlateau(self, aliens[-1].position())
+                else:
+                    plateau.afficherPlateau(self, (self.vaisseau.posX, -1))
+            else:
+                plateau.afficherPlateau(self)
+            if(self.finJeu(self.alienPlusBas())):
+                self.initialiserAliens(20 + 10 * self.niveau)
+            else:
+                self.deplacementAliens()
+            sleep(.1)
     
-    def finJeu(self):
-        if len(self.aliens) > 0:
-            self.vie -= 1
-            self.vaisseau.tir = 1
-        else:
+    def finJeu(self, alien: Alien) -> bool:
+        if len(self.aliens) < 1 :
             self.niveau += 1
+            return True
+        elif alien.posY == Plateau.hauteur - 1:
+            self.vie -= 1
+            return True
+        else:
+            return False
+            
 
     def affichageModeEmploi(self):
         print(PartieJeu.modeEmploi)
-class Alien:
-    alienC : str = "\033[1;32;47m"
-    def __init__(self, x:int, y:int, nivTir : int = 0):
-        self.posX = x
-        self.posY = y
-        self.sens = True
-        self.nivTir = nivTir
-    def __repr__(self):
-        return f'en position ({self.posX}, {self.posY}) - tir {self.nivTir}'
-    def __str__(self):
-        return f'{self.alienC}@' 
-
-    def position(self) -> Tuple[int, int]:
-        return (self.posX, self.posY)
 
 plateau : Plateau = Plateau()
 partie : PartieJeu = PartieJeu(Vaisseau(Plateau.largeur // 2))
 partie.affichageModeEmploi()
+partie.boucleDeJeu(plateau)
